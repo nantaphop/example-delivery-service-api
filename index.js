88a3ccd7d84170b2d4ff1de3e172a7c5bf35d356
Nantaphop Phuengphae <nantaphop.phuengphae@krungthai-axa.co.th>
2019-12-10 20:35:00 +0700

    connect to mongo db via docker-compose

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config()
+const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
 
+const MONGO_CONNECTION = process.env.MONGO_CONNECTION
+const PORT = process.env.PORT || 3000
+
+console.info('Connecting to Mongodb')
+const mongoOpts = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(MONGO_CONNECTION, mongoOpts, (err) => {
+    console.info('Connected to Mongodb')
+})
+
 require('./src/controllers/system/register')(app)
 
-const server = app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(PORT, () => {
     console.info(`listening at ${server.address().port}`)
 })
