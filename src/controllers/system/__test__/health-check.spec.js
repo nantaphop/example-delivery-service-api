85d74e72989208913af276cc27a19f0d8eab7392
Nantaphop Phuengphae <nantaphop.phuengphae@krungthai-axa.co.th>
2019-12-10 20:21:04 +0700

    Test health-check with sinon

--- src/controllers/system/__test__/health-check.spec.js
+++ src/controllers/system/__test__/health-check.spec.js
@@ -1,5 +1,27 @@
+const sinon = require('sinon')
+const healthCheck = require('../health-check')
+
 describe('health-check', () => {
-    it('should return jsom with ready', () => {
-        expect(1).toEqual(2)
+    const req = {}
+    const res = {
+        json: sinon.stub(),
+        status: sinon.stub()
+    }
+
+    beforeEach(() => {
+        res.json.reset()
+        res.status.reset()
+    })
+
+    it('should return json with ready', async () => {
+        await healthCheck(req, res)
+        // Not specific order 
+        expect(res.json.calledWith({ready: true})).toBeTruthy()
+    })
+
+    it('should return with status 200', async () => {
+        await healthCheck(req, res)
+        // Specific to first call arguments 
+        expect(res.status.getCall(0).args).toEqual([200])
     })
 })
