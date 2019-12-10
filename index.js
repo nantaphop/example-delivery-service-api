eb556d2a874874a155c7768d67550179b5b6fe02
Nantaphop Phuengphae <nantaphop.phuengphae@krungthai-axa.co.th>
2019-12-10 19:54:13 +0700

    Run api in docker-compose

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.json({hello: 'world'}))
+app.get('/', (req, res) => res.json({hello: 'world from docker'}))
 const server = app.listen(process.env.PORT || 3000, () => {
     console.info(`listening at ${server.address().port}`)
 })
