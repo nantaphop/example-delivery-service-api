8b3ce284a7ffeedf29c0180f7c3d03c9ccae9f8b
Nantaphop Phuengphae <nantaphop.phuengphae@krungthai-axa.co.th>
2019-12-10 21:19:44 +0700

    Update mongo env

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 const MONGO_CONNECTION = process.env.MONGO_CONNECTION
 const PORT = process.env.PORT || 3000
 
+// Config Middleware for req
+app.use(bodyParser.json())
+
+// Config route handlers
+require('./src/controllers/system/register')(app)
+require('./src/controllers/auth/register')(app)
+
 console.info('Connecting to Mongodb')
 const mongoOpts = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(MONGO_CONNECTION, mongoOpts, err => {
-  console.info('Connected to Mongodb')
+  if (err) console.error(`Can't connect to mongodb`, err)
+  else console.info('Connected to Mongodb')
 })
-
-require('./src/controllers/system/register')(app)
-
 const server = app.listen(PORT, () => {
   console.info(`listening at ${server.address().port}`)
 })
