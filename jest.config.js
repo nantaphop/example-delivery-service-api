f723f7b2598d576959f7d921724bda86c381b32e
Nantaphop Phuengphae <nantaphop.phuengphae@krungthai-axa.co.th>
2019-12-12 22:46:23 +0700

    add test coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 // https://jestjs.io/docs/en/configuration
 module.exports = {
-    testEnvironment: 'node'
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  collectCoverageFrom: ['src/**/*.js', '!src/controllers/**/register.js', '!src/test-utils/*.js']
 }
